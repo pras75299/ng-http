cb302f632b0e0a5ed52120b4bc0096d35d14ab93
pras75299 <singh.prashantking@gmail.com>
2019-01-01 17:04:01 +0530

    added router module in app module and added somenew component such as no found, home component and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './home/home.component';
 import { UserCommentService } from './services/user-comment.service';
 import { CommentService } from './services/comment.service';
 import { PostService } from './services/post.service';
@@ -10,6 +11,9 @@ import { CommentComponent } from './comment/comment.component';
 import { UsersComponent } from './users/users.component';
 import { UserServiceService } from './services/user-service.service';
 import { UserCommentComponent } from './user-comment/user-comment.component';
+import { NavComponent } from './nav/nav.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -17,11 +21,23 @@ import { UserCommentComponent } from './user-comment/user-comment.component';
     PostComponent,
     CommentComponent,
     UsersComponent,
-    UserCommentComponent
+    UserCommentComponent,
+    HomeComponent,
+    NavComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'post', component: PostComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'usersComment', component: UserCommentComponent },
+      { path: 'comment', component: CommentComponent  },
+      { path: '**', component: NotfoundComponent  }
+    ])
+    
   ],
   providers: [
     PostService,
